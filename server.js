355955b200cccaab7bf66072c110e4d97e13b911
Anton Valkov <digitall24@gmail.com>
2025-05-30 15:27:07 +0300

    server.js

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const nodemailer = require('nodemailer');
 const ftp = require('basic-ftp');
 const path = require("path");
 
-
 app.use(cors()); 
 app.use(express.json());
 app.use(express.static("public"));
+app.use("/downloads", express.static(path.join(__dirname)));
 
 
 function generateXml(eik, fdrid) {
